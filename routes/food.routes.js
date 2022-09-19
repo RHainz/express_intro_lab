b43013b6f224e735e21b9b556519d7501696efb8
RHainz <rogerhainz@gmail.com>
2022-09-19 20:03:19 -0300

    CRUD NOT NUD

--- routes/food.routes.js
+++ routes/food.routes.js
@@ -2,14 +2,18 @@ import express from "express"
 import data from "./data.js"
 
 const foodRouter=express.Router();
-const newData=[];
+let newData=[];
 let _id=0;
 let foodJect={
     "_id":"",
     "name":""
 }
+let foodJect2={}
 const count=Math.floor(Math.random()*99999999999);
 let counter=count;
+
+console.log("adios")
+
 
 function generateId () {
     let idNew=counter;
@@ -20,18 +24,12 @@ function generateId () {
 
 
 foodRouter.post("/create-food", (req,res) =>{
-    let naming=req.body.name;
+    foodJect=req.body
     let newId=generateId();
-    console.log (newId)
-    console.log(req.body);
     foodJect._id=newId;
-    foodJect.name=naming;
+    console.log(foodJect)
     data.push(foodJect);
-    foodJect={
-        "_id":"",
-        "name":""
-    }
-    
+    foodJect={}; 
     return res.json(data);
 })
 
@@ -39,13 +37,39 @@ foodRouter.get("/create-food",(req,res) =>{
     return res.json(data);
 })
 
+foodRouter.delete("/create-food/:id", (req,res)=>{
+    const id = req.params.id
+    console.log(data._id)
+    data.map((current)=>{
+        if (current._id != id) {
+        newData.push(current);
+        }
+    
+    })
+    data.splice(0, data.length);
+    newData.forEach((curr)=>data.push(curr))
+    newData.splice(0,newData.length);
+    return res.json (data);
+});
+
 foodRouter.put("/create-food/:id", (req,res)=>{
     const id = req.params.id
-    // data.map((current)=>{
-    //     if (current === `${name}`) {
-    //         data[current]=(req.body)}        
-    //   } 
+    console.log(req.body)
+    data.map((current)=>{
+        if (current._id != id) {
+        newData.push(current);
+        }
+    if (current._id == id) {
+        foodJect=current;
+        }
     })
-
+    Object.assign(foodJect,req.body);
+    data.splice(0, data.length);
+    newData.forEach((curr)=>data.push(curr));
+    data.push(foodJect);
+    foodJect={};
+    newData=[];
+    return res.json (data);
+});
 
 export {foodRouter};
